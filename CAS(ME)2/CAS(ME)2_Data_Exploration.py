7f9dd440846850249716171d8e20b9e0c4627246
NikinGeethila <nikingeethila@gmail.com>
2020-07-02 02:18:07 +0800

    Create CAS(ME)2_Data_Exploration.py

---
+++ CAS(ME)2/CAS(ME)2_Data_Exploration.py
@@ -0,0 +1,28 @@
+import os
+
+
+
+path='../../../Datasets/CAS(ME)2_categorical/'
+
+
+
+directorylisting = os.listdir(path)
+img_count=[]
+for cat in directorylisting:
+    # print(subject)`
+    subjectdirectorylisting=os.listdir(path+cat)
+    for video in subjectdirectorylisting:
+        videopath = path+cat +'/'+ video
+        # print(videopath)
+        imgs=os.listdir(videopath)
+        count=0
+        for img in imgs:
+            count+=1
+        print(count)
+        img_count.append(count)
+        # print(str(subjectdirectorylisting)+str(video))
+print(img_count)
+print(sum(img_count)/len(img_count))
+print(max(img_count))
+print(min(img_count))
+print(len(img_count))
